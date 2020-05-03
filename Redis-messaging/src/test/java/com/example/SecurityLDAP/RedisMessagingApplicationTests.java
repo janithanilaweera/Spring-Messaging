dd57479fe00b38f54ea311b7f360d93710b268e2
Jay <janitha999@gmail.com>
2020-05-03 10:43:28 +1000

    first one

---
+++ Redis-messaging/src/test/java/com/example/SecurityLDAP/RedisMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecurityLDAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
